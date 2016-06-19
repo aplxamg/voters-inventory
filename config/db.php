45da4ec7f109425fc3b00d935e68882c4ca2cedc
gabisan.am <amosqueragabisan@gmail.com>
2016-06-18 23:55:37 -0700

    Remove unused files

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=_commondb',
-    'username' => 'root',
-    'password' => 'dimensions',
-    'charset' => 'utf8',
-];
